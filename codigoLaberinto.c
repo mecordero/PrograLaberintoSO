835f631acda26822682eb1b46397d5e5588554ba
ygomez <yanil.gomeznav@gmail.com>
2019-04-03 10:40:26 -0600

    struct celda
    info individual de cada celda y imprimir el archivo pero solo me pone *

--- codigoLaberinto.c
+++ codigoLaberinto.c
@@ -1,6 +1,19 @@
 
-
 #include <stdio.h>
+#include <stdlib.h>
+
+
+enum Tipo_Celda {PARED, FINAL, LIBRE} tipo;
+
+struct celda {
+	char caracter;
+	char arriba ;
+	char abajo ;
+	char derecha;
+	char izquierda;
+	enum Tipo_Celda tipo;
+	//sem_t semaforo;
+	};
 
 int main(int argc, char **argv)
 {
@@ -44,18 +57,46 @@ int main(int argc, char **argv)
 	printf("Filas: %d\n", filas);
 	printf("Columnas: %d\n", columnas);
 		
-	char laberinto[filas][columnas];	
+	struct celda laberinto[filas][columnas];	
 	
 	for(int i = 0; i < filas; i ++){
 		for(int j = 0; j < columnas; j++){
-			laberinto[i][j] = '*';
+			laberinto[i][j].caracter = '*';
+			laberinto[i][j].arriba = '.';
+			laberinto[i][j].abajo = '.';
+			laberinto[i][j].derecha = '.';
+			laberinto[i][j].izquierda= '.';
+			tipo = PARED;
+			//sem_init(&laberinto[i][j].semaforo, 0, 0);
 		}
 	}
 	
+	int i = 0;
+	int j = 0;
+	
+	while ( feof(fileLaberinto) ) {
+		while ((charActual = fgetc(fileLaberinto)) != '\n'){
+			laberinto[i][j].caracter = charActual;
+			switch (charActual) {
+				case '/':
+					laberinto[i][j].tipo = FINAL;
+					break;
+				case ' ':
+					laberinto[i][j].tipo = LIBRE;
+					break;
+				default:
+					laberinto[i][j].tipo = PARED;
+					break;					
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
 	
 	for(int i = 0; i < filas; i ++){
 		for(int j = 0; j < columnas; j++){
-			printf("%c\t", laberinto[i][j]);
+			printf("%c\t", laberinto[i][j].caracter);
 		}
 		printf("\n");
 	}
@@ -77,4 +118,3 @@ int main(int argc, char **argv)
 	
 	return 0;
 }
-
